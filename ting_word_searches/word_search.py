7968c32e7b4151bea1c9507075b1fb49a99b3a95
Thiago <thiago-470@outlook.com>
2023-06-15 16:44:02 -0300

    req07

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,24 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    words = []
+    for i in range(len(instance)):
+        file_name = instance.search(i)["nome_do_arquivo"]
+        lines = instance.search(i)["linhas_do_arquivo"]
+        occurrences = []
+
+    for index, line in enumerate(lines):
+        if word.lower() in line.lower():
+            occurrence = {"linha": index + 1}
+            occurrences.append(occurrence)
+
+    if len(occurrences) > 0:
+        word_info = {
+            "palavra": word,
+            "arquivo": file_name,
+            "ocorrencias": occurrences
+        }
+        words.append(word_info)
+
+    return words
 
 
 def search_by_word(word, instance):
