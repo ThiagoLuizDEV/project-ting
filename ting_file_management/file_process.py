27366cb026a1a8857c18d5437358f306b6a8586a
Thiago <thiago-470@outlook.com>
2023-06-14 22:51:56 -0300

    req03

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+import sys
+from .file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file = txt_importer(path_file)
+    dictionary = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file,
+    }
+    for i in range(len(instance)):
+        result = instance.search(i)
+        if result == dictionary:
+            return None
+    instance.enqueue(dictionary)
+    print(dictionary, file=sys.stdout)
 
 
 def remove(instance):
